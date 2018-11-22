28bdd0b33aa676e5bc89188d964c2428030f97f9
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-11-22 19:12:47 +0900

    Notes Route

---
+++ src/Route/Notes/Notes.js
@@ -0,0 +1,94 @@
+import React from "react"
+import { Query } from "react-apollo"
+import styled from "styled-components"
+import { GET_NOTES } from "../../queries";
+import { Link } from "react-router-dom";
+import { ReactComponent as Add } from "../../Components/add.svg";
+
+
+const Header = styled.div`
+    margin-bottom: 50px;
+`;
+
+const Title = styled.h1`
+    font-size: 50px;
+    font-weight: 600;
+    margin: 0;
+    margon-bottom: 30px;
+    display: flex;
+    aligh-items: center;
+`;
+
+const Button = styled.div`
+    margin-left: 10px;
+    transform: scale(0.8);
+    background-color: #eee;
+    display: flex;
+    aligh-items: center;
+    padding: 10px;
+    border-radius: 10px;
+    cursor: pointer;
+`;
+
+const Subtitle = styled.h2`
+    color: #a2a19e;
+    font-weight: 400;
+`;
+
+const Notes = styled.div`
+    display: flex;
+    flex-direction: column;
+    aligh-item: felx-start;
+`;
+const Note = styled.div`
+    padding: 10px;
+    padding-left: 5px;
+    transition: background-color 0.1s linear;
+    cursor: pointer;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    &:hover {
+        background-color: #eeeeee;
+    }
+`;
+
+const NoteTitle = styled.span`
+    padding-bottom: 5px;
+    font-weight: 600;
+    font-size: 20px;
+`;
+
+
+export default class NoteContainer extends React.Component{
+    render() {
+        return (
+            <>
+            <Header>
+                <Title>
+                    Offline Note
+                    <Link to={"/add"}>
+                        <Button>
+                            <Add />
+                        </Button>
+                    </Link>
+                </Title>
+                <Subtitle>Taking Notes while we learn.</Subtitle>
+            </Header>
+            <Notes>
+            <Query query={GET_NOTES}>
+                {({data}) => 
+                    data.notes ? 
+                    data.notes.map(note =>( 
+                        <Link to={`/note/${note.id}`} key={note.id}>
+                            <Note>
+                                <NoteTitle>{note.title}</NoteTitle>
+                            </Note>
+                        </Link>
+                    )) : null
+                }
+            </Query>
+            </Notes>
+            </>
+        )
+    }
+}
