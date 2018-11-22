2289b6ce08fc986efd9bf0f8a94db2b4f5207187
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-11-22 14:11:25 +0900

    query error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Query } from "react-apollo";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        {/* <Query query={}>{() => null}</Query> */}
       </div>
     );
   }
