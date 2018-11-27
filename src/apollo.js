112f6bac3799c9ea280b710479a0750521362a7c
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-11-27 12:52:26 +0900

    Apollo

--- src/apollo.js
+++ src/apollo.js
@@ -1,22 +1,22 @@
 import { ApolloClient } from "apollo-client";
-import { InMemoryCache} from "apollo-cache-inmemory"
-import { withClientState } from "apollo-link-state"
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { withClientState } from "apollo-link-state";
 import { ApolloLink } from "apollo-link";
 
-import { typeDefs, defaults, resolvers} from "./clientState"
+import { typeDefs, defaults, resolvers } from "./clientState";
 
 const cache = new InMemoryCache();
 
 const stateLink = withClientState({
-    cache,
-    typeDefs,
-    defaults,
-    resolvers
+  cache,
+  typeDefs,
+  defaults,
+  resolvers
 });
 
 const client = new ApolloClient({
-    cache,
-    link : ApolloLink.from([stateLink])
-})
+  cache,
+  link: ApolloLink.from([stateLink])
+});
 
-export default client
+export default client;
